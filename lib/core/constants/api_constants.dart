3e88ba3d3f33a83a3be076a47a0b3c2f99deee61
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2025-06-22 21:13:47 +0100

    version en ligne

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,20 +1,20 @@
 class ApiConstants {
-  static const String baseIp = '10.226.2.2';
-  static const String basePort = '8079';
+  static const String baseIp = '109.199.113.94';
+  static const String basePort = '30079';
 
   static const String baseUrl = 'http://$baseIp:$basePort';
 
   // Services
-  static const String authService = '$baseUrl/SERVICE-AUTH';
+  static const String authService = '$baseUrl/service-auth';
 
   // Endpoints
   static const String login = '$authService/auth/login';
-  static const String register = '$baseUrl/SERVICE-USERS/graphql';
-  static const String uploadCni = '$baseUrl/SERVICE-USERS/upload/cni';
-  static const String accountServiceUrl = '$baseUrl/SERVICE-ACCOUNT/graphql';
-  static const String cardServiceUrl = '$baseUrl/SERVICE-CARD/graphql';
-  static const String bankServiceUrl = '$baseUrl/SERVICE-BANKS/graphql';
-  static const String transactionServiceUrl = '$baseUrl/SERVICE-TRANSACTIONS/graphql';
-  static const String transfertServiceUrl = '$baseUrl/SERVICE-TRANSFERT/graphql';
-  static const String rechargeRetraitServiceUrl = '$baseUrl/SERVICE-RECHARGE-RETRAIT/graphql';
+  static const String register = '$baseUrl/service-users/graphql';
+  static const String uploadCni = '$baseUrl/service-users/upload/cni';
+  static const String accountServiceUrl = '$baseUrl/service-account/graphql';
+  static const String cardServiceUrl = '$baseUrl/service-card/graphql';
+  static const String bankServiceUrl = '$baseUrl/service-banks/graphql';
+  static const String transactionServiceUrl = '$baseUrl/service-transaction/graphql';
+  static const String transfertServiceUrl = '$baseUrl/service-transfert/graphql';
+  static const String rechargeRetraitServiceUrl = '$baseUrl/service-recharge-retrait/graphql';
 }
